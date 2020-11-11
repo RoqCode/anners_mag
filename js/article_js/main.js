74e673f3f5d6d1becf5363bec8897210925b46fb
RoqCode <74173062+RoqCode@users.noreply.github.com>
2020-11-11 20:32:39 +0100

    VH fix

--- js/article_js/main.js
+++ js/article_js/main.js
@@ -112,3 +112,13 @@ $(document).scroll(function () {
   showBackButton();
   didScroll = true;
 });
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+$(window).resize(function () {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
